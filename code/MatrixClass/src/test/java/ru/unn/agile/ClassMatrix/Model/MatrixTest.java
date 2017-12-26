8c9687cf4052b10b76e95577514f85f32fe7d396
andrewaverin <33939084+andrewaverin@users.noreply.github.com>
2017-12-26 23:43:23 +0300

    Update MatrixTest.java

--- code/MatrixClass/src/test/java/ru/unn/agile/ClassMatrix/Model/MatrixTest.java
+++ code/MatrixClass/src/test/java/ru/unn/agile/ClassMatrix/Model/MatrixTest.java
@@ -1,137 +1,177 @@
 package ru.unn.agile.ClassMatrix.Model;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
 
 public class MatrixTest {
 
-    private Matrix someIntMatrix;
+    @Test
+    public void whenMatrixNull() {
+        try {
+            Matrix testMatrix = new Matrix(null);
+            Assert.fail();
+        } catch (IllegalStateException ex) {
+        }
+    }
 
     @Test
-    public void whenIfMatrixInt2x2Quadriq() {
-        int[][]arrayForCreateIntMatrix2x2 = {{1, 2, },
-                                               {3, 5 }
+    public void whenMatrix2x2IsCreatedSuccessful() {
+        int[][] array = {{1, 2},
+                {3, 5}
         };
 
-        someIntMatrix = new Matrix(arrayForCreateIntMatrix2x2);
-
-        assertTrue(someIntMatrix.matrixIsQuadric());
+        Matrix testMatrix = getTestMatrixFromValidArray(array);
+        assertTrue(testMatrix.getNumberColumn() == 2);
+        assertTrue(testMatrix.getNumberRow() == 2);
     }
 
     @Test
-    public void whenIfMatrixInt3x2IsNotQuadriq() {
-        int[][]arrayForCreateIntMatrix3x2 = {{1, 2, },
-                                               {3, 5, },
-                                               {1, 5 }
+    public void whenMatrixWithWrongRowsAndColumns() {
+        int[][] array = {{1, 2},
+                {3, 5, 3},
+                {5}
         };
 
-        someIntMatrix = new Matrix(arrayForCreateIntMatrix3x2);
-
-        assertFalse(someIntMatrix.matrixIsQuadric());
+        try {
+            Matrix testMatrix = new Matrix(array);
+            Assert.fail();
+        } catch (IllegalStateException ex) {
+        }
     }
 
     @Test
-    public void initiliseMatrix2x2WhenSomeIntNumbers() {
-        int[][]expectedSomeTestMatrix2x2 = {{1, 2, },
-                                           {3, 5 }
-                                                 };
-
-        int[][]arrayForCreateIntMatrix2x2 = {{1, 2, },
-                                               {3, 5 }
-                                                        };
-
-        someIntMatrix = new Matrix(arrayForCreateIntMatrix2x2);
-        int[][]getSomeIntMatric = someIntMatrix.getMatrix();
-
-        assertArrayEquals(expectedSomeTestMatrix2x2, getSomeIntMatric);
-    }
-
-    @Test
-    public void initiliseMatrix3x3WhenSomeIntNumbers() {
-        int[][]expectedSomeTestMatrix3x3 = {{1, 2, 3 },
-                                           {3, 5, 4 },
-                                           {1, 5, 4 }
+    public void whenMatrix2x2WhenSomeIntNumbers() {
+        int[][] array = {{1, 2},
+                {3, 5}
         };
 
-        int[][]arrayForCreateIntMatrix3x3 = {{1, 2, 3 },
-                                               {3, 5, 4 },
-                                               {1, 5, 4 }
-        };
-
-        someIntMatrix = new Matrix(arrayForCreateIntMatrix3x3);
-        int[][]getSomeIntMatric = someIntMatrix.getMatrix();
-
-        assertArrayEquals(expectedSomeTestMatrix3x3, getSomeIntMatric);
+        Matrix testMatrix = getTestMatrixFromValidArray(array);
+        assertArrayEquals(testMatrix.toArray(), array);
     }
 
     @Test
-    public void initiliseMatrix3x2WhenSomeIntNumbers() {
-        int[][]expectedSomeTestMatrix3x2 = {{1, 2, },
-                                           {3, 5, },
-                                           {1, 5 }
+    public void whenMatrix2x2IsQuadriq() {
+        int[][] array = {{1, 2},
+                {3, 5}
         };
 
-        int[][]arrayForCreateIntMatrix3x2 = {{1, 2, },
-                                               {3, 5, },
-                                               {1, 5 }
+        Matrix testMatrix = getTestMatrixFromValidArray(array);
+        assertTrue(testMatrix.isQuadric());
+    }
+
+
+    @Test
+    public void whenMatrix2x3NotQuadriq() {
+        int[][] array = {{1, 2, 3},
+                {3, 5, 9}
         };
 
-        someIntMatrix = new Matrix(arrayForCreateIntMatrix3x2);
-        int[][]getSomeIntMatric = someIntMatrix.getMatrix();
+        Matrix testMatrix = getTestMatrixFromValidArray(array);
+        assertTrue(!testMatrix.isQuadric());
+    }
 
-        assertArrayEquals(expectedSomeTestMatrix3x2, getSomeIntMatric);
+
+    @Test
+    public void whenMatrix3x2NotQuadriq() {
+        int[][] array = {{1, 2},
+                {3, 5},
+                {1, 5}
+        };
+
+        Matrix testMatrix = getTestMatrixFromValidArray(array);
+        assertFalse(testMatrix.isQuadric());
     }
 
     @Test
-    public void canCalculateDeterminantIntMatrix2x2WithResult18() {
-        int[][]someTestMatrix2x2 = {{6, 4, },
-                                       {3, 5 }
-                                             };
-
-        someIntMatrix = new Matrix(someTestMatrix2x2);
-
-        int calculateIntMatrixDeterminant = someIntMatrix.determinant(
-                someIntMatrix.getMatrix(),
-                someIntMatrix.getColumns());
-
-        int expectedDeterminantResult = 18;
-
-        assertEquals(expectedDeterminantResult, calculateIntMatrixDeterminant);
-    }
-
-    @Test
-    public void areTrueWhenCalculateDeterminantIntMatrix3x2() {
-        int[][]someTestMatrix3x2 = {{6, 4, },
-                                      {3, 5, },
-                                      {6, 5 }
-                                              };
-
-        someIntMatrix = new Matrix(someTestMatrix3x2);
-
-        int calculateIntMatrixDeterminant = someIntMatrix.determinant(
-                someIntMatrix.getMatrix(),
-                someIntMatrix.getColumns());
-
-        assertTrue(calculateIntMatrixDeterminant == 0);
-    }
-
-    @Test
-    public void canCalculateDeterminantIntMatrix3x3WithResult1() {
-        int[][]someTestMatrix3x3 = {{1, 0, 1, },
-                                      {0, 1, 0, },
-                                      {0, 0, 1},
+    public void whenInitializationMatrix3x3SomeIntNumbersIsCorrect() {
+        int[][] array = {{1, 2, 3},
+                {3, 5, 4},
+                {1, 5, 4}
         };
 
-        someIntMatrix = new Matrix(someTestMatrix3x3);
-
-        int calculateIntMatrixDeterminant = someIntMatrix.determinant(
-                someIntMatrix.getMatrix(),
-                someIntMatrix.getColumns());
-
-        int expectedDeterminantResult = 1;
-
-        assertEquals(expectedDeterminantResult, calculateIntMatrixDeterminant);
+        Matrix testMatrix = getTestMatrixFromValidArray(array);
+        assertArrayEquals(array, testMatrix.toArray());
     }
 
+    @Test
+    public void whenInitializationMatrix3x2SomeIntNumbersIsCorrect() {
+        int[][] array1 = {{1, 2},
+                {3, 5},
+                {5, 5}
+        };
+
+        int[][] array2 = {{1, 2},
+                {3, 5},
+                {5, 5}
+        };
+
+        Matrix testMatrix = getTestMatrixFromValidArray(array1);
+        assertArrayEquals(testMatrix.toArray(), array2);
+    }
+
+    @Test
+    public void whenMatrixWith1ElementCalculateDeterminant() {
+        int[][] array = {{777}};
+
+        Matrix testMatrix = getTestMatrixFromValidArray(array);
+        assertEquals(777, testMatrix.calculateDeterminant());
+    }
+
+    @Test
+    public void whenCalculateDeterminantIntMatrix2x2WithResult18() {
+        int[][] array = {{6, 4},
+                {3, 5}
+        };
+
+        Matrix testMatrix = getTestMatrixFromValidArray(array);
+        assertEquals(18, testMatrix.calculateDeterminant());
+    }
+
+    @Test
+    public void whenTrueCalculateDeterminantIntMatrix3x2() {
+        int[][] array = {{6, 4},
+                {3, 5},
+                {6, 5}
+        };
+
+        Matrix testMatrix = getTestMatrixFromValidArray(array);
+        assertTrue(testMatrix.calculateDeterminant() == 0);
+    }
+
+    @Test
+    public void whenCalculateDeterminantIntMatrix3x3WithResult1() {
+        int[][] array = {{1, 0, 1},
+                {0, 1, 0},
+                {0, 0, 1},
+        };
+
+        Matrix testMatrix = getTestMatrixFromValidArray(array);
+        assertEquals(1, testMatrix.calculateDeterminant());
+    }
+
+    @Test
+    public void whenCalculateDeterminantIntDegenerateMatrix3x3WithResult1() {
+        int[][] array = {{1, 2, 4},
+                {1, 1, 1},
+                {1, 1, 1},
+        };
+
+        Matrix testMatrix = getTestMatrixFromValidArray(array);
+        assertEquals(testMatrix.calculateDeterminant(), 0);
+    }
+
+    private Matrix getTestMatrixFromValidArray(final int[][] array) {
+        Matrix testMatrix = null;
+        try {
+            testMatrix = new Matrix(array);
+        } catch (IllegalStateException ex) {
+            Assert.fail();
+        }
+        return testMatrix;
+    }
 }
