27cebbd1a9470d9951a3cc0d6559d26a6dce3b68
andrewaverin <33939084+andrewaverin@users.noreply.github.com>
2017-12-26 23:41:34 +0300

    Update Matrix.java

--- code/MatrixClass/src/main/java/ru/unn/agile/ClassMatrix/Model/Matrix.java
+++ code/MatrixClass/src/main/java/ru/unn/agile/ClassMatrix/Model/Matrix.java
@@ -2,85 +2,106 @@ package ru.unn.agile.ClassMatrix.Model;
 
 public class Matrix {
 
-    private int[][]newMatrix;
-    private int n;
-    private int m;
-
-    public Matrix(final int[][]dataMassive) {
-        this.n = dataMassive.length;
-        this.m = dataMassive[0].length;
-        newMatrix = new int[n][m];
-        newMatrix = dataMassive.clone();
+    public static class FailedArrayException extends Exception {
     }
 
-    public int[][] getMatrix() {
-        return newMatrix;
-    }
-
-    public boolean matrixIsQuadric() {
-        return n == m;
-    }
-
-   private void getMatrixWithoutRowAndCol(final int[][]matrix,
-                                          final int size,
-                                          final int row,
-                                          final int col,
-                                          final int[][]newMatrix) {
-       int offsetRow = 0;
-       int offsetCol = 0;
-       for (int i = 0; i < size - 1; i++) {
-           if (i == row) {
-               offsetRow = 1;
-           }
-           offsetCol = 0;
-           for (int j = 0; j < size - 1; j++) {
-               if (j == col) {
-                   offsetCol = 1;
-               }
-               newMatrix[i][j] = matrix[i + offsetRow][j + offsetCol];
-           }
-       }
-   }
-
-    public int determinant(final int[][]forDeterminantMatrix, final int size) {
-        int  result, degree;
-        result = 0;
-        degree = 1;
-            if (matrixIsQuadric()) {
-                if (size == 1) {
-                    result = forDeterminantMatrix[0][0];
-                    return result;
-                }
-
-                if (size == 2) {
-                    int a1 = forDeterminantMatrix[0][0] * forDeterminantMatrix[1][1];
-                    int a2 = forDeterminantMatrix[0][1] * forDeterminantMatrix[1][0];
-                    result =  a1 - a2;
-                    return result;
-                }
-
-                if (n > 2) {
-                    int[][] p = new int[n - 1][n - 1];
-                    for (int j = 0; j < size; j++) {
-                        getMatrixWithoutRowAndCol(forDeterminantMatrix, size, 0, j, p);
-                        int a1j = forDeterminantMatrix[0][j];
-                        int resultEndPart = degree * a1j * determinant(p, size - 1);
-                        result = result + (resultEndPart);
-                        degree = -degree;
-                    }
-                }
-
-            } else {
-                return 0;
+    public Matrix(final int[][] array) throws IllegalStateException {
+        if (array == null) {
+            throw new IllegalStateException("Input array is empty!");
+        }
+        row = array.length;
+        column = array[0].length;
+        for (int i = 1; i < row; i++) {
+            if (array[i].length != column) {
+                throw new IllegalStateException("Column have different number or null!");
             }
+        }
+
+        matrixData = array.clone();
+    }
+
+    public int[][] toArray() {
+        return matrixData;
+    }
+
+    public boolean isQuadric() {
+        return row == column;
+    }
+
+    public int calculateDeterminant() {
+        int result = 0;
+        if (isQuadric()) {
+            result = computeDeterminant(matrixData, row);
+        }
         return result;
     }
 
-    public int getColumns() {
-        return n;
+    public int getNumberRow() {
+        return row;
     }
 
-    public int getRows() {
-        return m;
+    public int getNumberColumn() {
+        return column;
     }
+
+    private static void getMatrixWithoutRowAndCol(final int[][] matrix,
+                                                  final int size,
+                                                  final int excludedRow,
+                                                  final int excludedColumn,
+                                                  final int[][] array) {
+
+        if (matrix == null) {
+            throw new IllegalStateException("Matrix is empty!");
+        }
+
+        int row = 0;
+        int col = 0;
+        for (int i = 0; i < size - 1; i++) {
+            if (i == excludedRow) {
+                row = 1;
+            }
+            col = 0;
+            for (int j = 0; j < size - 1; j++) {
+                if (j == excludedColumn) {
+                    col = 1;
+                }
+                array[i][j] = matrix[i + row][j + col];
+            }
+        }
+    }
+
+    private static int computeDeterminant(final int[][] array, final int size) {
+        int result;
+        int signMinor;
+        result = 0;
+        signMinor = 1;
+
+        if (size == 1) {
+            result = array[0][0];
+            return result;
+        }
+
+        if (size == 2) {
+            int mainDiagonal = array[0][0] * array[1][1];
+            int sideDiagonal = array[0][1] * array[1][0];
+            result = mainDiagonal - sideDiagonal;
+            return result;
+        }
+
+        if (size > 2) {
+            int[][] p = new int[size - 1][size - 1];
+            for (int j = 0; j < size; j++) {
+                getMatrixWithoutRowAndCol(array, size, 0, j, p);
+                int ratio = array[0][j];
+                int resultEndPart = signMinor * ratio * computeDeterminant(p, size - 1);
+                result = result + resultEndPart;
+                signMinor = -signMinor;
+            }
+        }
+        return result;
+    }
+
+    private int[][] matrixData;
+    private int row;
+    private int column;
 }
