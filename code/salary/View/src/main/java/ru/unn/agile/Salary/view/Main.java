43181007e57ddf3e57fe56956fea34d5d9db91d7
andrewaverin <33939084+andrewaverin@users.noreply.github.com>
2018-02-01 17:48:17 +0300

    Update Main.java

--- code/salary/View/src/main/java/ru/unn/agile/Salary/view/Main.java
+++ code/salary/View/src/main/java/ru/unn/agile/Salary/view/Main.java
@@ -7,11 +7,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(final Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Calculator.fxml"));
-        primaryStage.setTitle("Calculator");
+        Parent root = FXMLLoader.load(getClass().getResource("SalaryCalculator.fxml"));
+        primaryStage.setTitle("SalaryCalculator");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
